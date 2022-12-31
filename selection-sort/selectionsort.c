d71c7c003c72eaca7015f2114a9a6efe39c52ea7
Your Tech Guy <tanbirramim420@gmail.com>
2023-01-01 03:09:14 +0600

    description added

--- selection-sort/selectionsort.c
+++ selection-sort/selectionsort.c
@@ -1,4 +1,14 @@
 // selection sort algorithm
+// 1. Find the smallest item in the array and exchange it with the first entry
+// 2. Find the next smallest item and exchange it with the second entry
+// 3. Continue in this way until the entire array is sorted
+// 4. This method is called selection sort because it works by repeatedly selecting the smallest remaining item
+// 5. The running time of selection sort is quadratic
+// 6. It makes ~ N^2/2 compares and ~ N exchanges
+// 7. Selection sort is insensitive to input
+// 8. It is useful when memory write is a costly operation
+// 9. It is also useful when the input is in the form of a stream
+
 #include <stdio.h>
 #include <stdlib.h>
 int main()
