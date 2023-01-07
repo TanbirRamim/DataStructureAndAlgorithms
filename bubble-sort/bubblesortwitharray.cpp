4db43fee3bb4c58382b1115cb90e48c7f2ca4fd8
Your Tech Guy <tanbirramim420@gmail.com>
2023-01-07 23:28:48 +0600

    bubble sort added

---
+++ bubble-sort/bubblesortwitharray.cpp
@@ -0,0 +1,30 @@
+// bubble sort algorithm
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
