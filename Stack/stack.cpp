a729e123cac8ab631601f0125c24e0a20cb00a04
Your Tech Guy <tanbirramim420@gmail.com>
2023-01-13 00:32:05 +0600

    structured

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -1,4 +1,11 @@
 // stack
+// stack is a linear data structure which follows a particular order in which the operations are performed.
+// The order may be LIFO(Last In First Out) or FILO(First In Last Out).
+// Mainly the following three basic operations are performed in the stack:
+// Push: Adds an item in the stack. If the stack is full, then it is said to be an Overflow condition.
+// Pop: Removes an item from the stack. The items are popped in the reversed order in which they are pushed. If the stack is empty, then it is said to be an Underflow condition.
+// Peek or Top: Returns top element of stack.
+
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 1000
