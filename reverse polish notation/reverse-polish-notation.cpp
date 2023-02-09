55ec2ff8c5965bd949ca31bb19243893689e1459
Your Tech Guy <tanbirramim420@gmail.com>
2023-02-10 03:01:50 +0600

    long ago

---
+++ reverse polish notation/reverse-polish-notation.cpp
@@ -0,0 +1,46 @@
+// reverse polish notation using stack for multiple digit numbers
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            st.push(s[i] - '0');
+        }
+        else
+        {
+            int op2 = st.top();
+            st.pop();
+            int op1 = st.top();
+            st.pop();
+            switch (s[i])
+            {
+            case '+':
+                st.push(op1 + op2);
+                break;
+            case '-':
+                st.push(op1 - op2);
+                break;
+            case '*':
+                st.push(op1 * op2);
+                break;
+            case '/':
+                st.push(op1 / op2);
+                break;
+            case '^':
+                st.push(pow(op1, op2));
+                break;
+            case '%':
+                st.push(op1 % op2);
+                break;
+            }
+        }
+    }
+    cout << st.top();
+    return 0;
+}
